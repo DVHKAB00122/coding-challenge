1473b182fd6a55f1d0993438bf0b8b6dc9c74e86
dvhkab001 <dvhkab001@cs.uct.ac.za>
2021-11-26 20:54:30 +0200

    version 3

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='search_term_data',
     packages=['search_term_data'],
-    version='0.2',
+    version='0.3',
     license='MIT',
     description='Python deamon that monitors a directory for new csv files.',
     author='Kabelo Davhana',
     author_email='eugene.davhana@gmail.com',
     url='https://github.com/DVHKAB00122/coding-challenge',
-    download_url='https://github.com/DVHKAB00122/coding-challenge/archive/refs/tags/v_02.tar.gz',
+    download_url='https://github.com/DVHKAB00122/coding-challenge/archive/refs/tags/v_03.tar.gz',
     keywords=['Monitors', 'daemon', 'csv'],
     install_requires=[
         'python-daemon', 'PyYAML'
